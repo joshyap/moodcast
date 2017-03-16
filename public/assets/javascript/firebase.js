ba0479d07b4df0ea31368f3d2294e5385d1e4770
Owner <Owner>
2017-03-15 20:05:53 -0500

    trying to update test-branch

--- public/assets/javascript/firebase.js
+++ public/assets/javascript/firebase.js
@@ -1,3 +1,11 @@
+  //setup an on click event to sign out the user
+ $('#sign-out').on('click', function() {
+   console.log('working');
+   firebase.auth().signOut();
+   var href = 'index.html';
+   window.location = href;
+ })
+
 
 
   // Initialize Firebase
@@ -11,55 +19,49 @@
 
   firebase.initializeApp(config);
 
-var database = firebase.database();
+  var database = firebase.database();
 
-// Initial Values
-//var initialBid = 0;
-//var name = '';
+firebase.auth().onAuthStateChanged(function(user) {
+ if (user) {
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
 
-// --------------------------------------------------------------
+   // var user = firebase.auth().currentUser.displayName;
+   // console.log('displayName 2 ' + user);
 
-// At the initial load, get a snapshot of the current data.
-database.ref().on("value", function(snapshot) {
+   // At the initial load, get a snapshot of the current data.
+   database.ref('/users/' + displayName).on("value", function(snapshot) {
 
-  console.log(snapshot.val());
+   console.log('value added ' + snapshot.val());
 
-  
+  //If any errors are experienced, log them to console.
+   }, function(errorObject) {
+          console.log("The read failed: " + errorObject.code);
+      });
 
+   // --------------------------------------------------------------
 
-  // If any errors are experienced, log them to console.
-}, function(errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
-
-// --------------------------------------------------------------
-
-// This is our API key
+   // This is our API key
     var APIKey = "166a433c57516f51dfab1f7edaed8413";
     // Here we are building the URL we need to query the database
     //var queryURL = "http://api.openweathermap.org/data/2.5/weather?" + "q=Bujumbura,Burundi&units=imperial&appid=" + APIKey;
     //var searchLocation = $("#selectedLocation").val().trim();
     var searchLocation = "houston";
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?" + "q=" + searchLocation + "&units=imperial&appid=" + APIKey;
-//start of button
-$("#btnSelectLocation").on("click", function(event) {
-  event.preventDefault();
-  searchLocation = $("#selectedLocation").val().trim();
-  //console.log("Location: " + searchLocation);
-  queryURL = "http://api.openweathermap.org/data/2.5/weather?" + "q=" + searchLocation + "&units=imperial&appid=" + APIKey;
-  //console.log(queryURL);
-//});
-/*
-          $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-          console.log("City: " + response.name);
-          $(".wind").html("Wind Speed: " + response.wind.speed);
-          console.log("Wind speed: " + response.wind.speed);
-          $(".humidity").html("Humidity: " + response.main.humidity);
-          console.log("Humidity: " + response.main.humidity);
-          $(".temp").html("Temperature (F) " + response.main.temp);
-          console.log("Temperature: " + response.main.temp);
-*/
-//end of button
+   //start of button
+
+  $("#btnSelectLocation").on("click", function(event) {
+    event.preventDefault();
+    searchLocation = $("#selectedLocation").val().trim();
+
+    queryURL = "http://api.openweathermap.org/data/2.5/weather?" + "q=" + searchLocation + "&units=imperial&appid=" + APIKey;
+
     // Here we run our AJAX call to the OpenWeatherMap API
     $.ajax({
         url: queryURL,
@@ -67,26 +69,87 @@ $("#btnSelectLocation").on("click", function(event) {
       })
       // We store all of the retrieved data inside of an object called "response"
       .done(function(response) {
-        // Log the queryURL
         console.log(queryURL);
-        // Log the resulting object
         console.log(response);
-        // Transfer content to HTML
-        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-        $(".wind").html("Wind Speed: " + response.wind.speed);
-        $(".humidity").html("Humidity: " + response.main.humidity);
-        $(".temp").html("Temperature (F): " + response.main.temp);
-        $(".rain").html("Precipitation: " + response.weather[0].main);
-        // Log the data in the console as well
-        //console.log("Wind Speed: " + response.wind.speed);
-        //console.log("Humidity: " + response.main.humidity);
-        //console.log("Temperature (F): " + response.main.temp);
 
-      // Save the new city in Firebase
-        database.ref().push({
-        name: response.name,
-        });
+         var searchName = $('#selectedLocation').val().trim().toLowerCase();
+         var name = response.name.toLowerCase();
 
-  
-        });
-      }); 
+        var temp = response.main.temp;
+        console.log('temp ' + temp);
+
+        var prec = response.weather[0].main;
+        console.log('precipitation ' + prec);
+
+        var lati = response.coord.lat;
+        console.log('latitude ' + lati);
+
+        var long = response.coord.lon;
+        console.log('longitude ' + long);
+
+        //create a temporary object to hold new playlist data
+
+        var newPlaylist = {
+          city: name,
+          temperature: temp,
+          precipitation: prec,
+          latitude: lati,
+          longitude: long
+        }
+
+         //use underscore to compare entered name and search result
+         var nameTest = _.isEqual(searchName, name);
+
+
+         //conditional to check it the response name matches the entered name
+         if(!nameTest) {
+            alert('invalid city, please try again');
+         }
+
+         // This conditional triggers when the rendered city name matches the city name entered
+         else {
+
+              // Transfer content to HTML
+              $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+              $(".wind").html("Wind Speed: " + response.wind.speed);
+              $(".humidity").html("Humidity: " + response.main.humidity);
+              $(".temp").html("Temperature (F): " + response.main.temp);
+              $(".rain").html("Precipitation: " + response.weather[0].main);
+
+              // Save the new city in Firebase
+              database.ref('/users/' + displayName).push(newPlaylist);
+         } 
+
+         //function for AJAX 404       
+      }).fail(function(error) {
+        alert('Invalid city, please try again');
+         });
+  }); 
+
+   
+ //set up an event to append the data to the screen when a child is added to the database
+ database.ref('/users/' + displayName).on('child_added', function(childSnapshot) {
+
+  //store everything into variables
+  var city = childSnapshot.val().city;
+  var temp = childSnapshot.val().temperature;
+  var prec = childSnapshot.val().precipitation;
+  var lat = childSnapshot.val().latitude;
+  var lon = childSnapshot.val().longitude;
+
+
+  //append the user data to the table
+  $('.table').append('<tr><td>' + city + '</td><td>' + temp + '</td><td>' + prec + '</td><td>'
+   + lat + '</td><td>' + lon + '</td></tr>');
+
+ })
+
+  } else {
+    // User is signed out.
+    // ...
+  }
+});
+
+//   $('button').on('click', function() {   
+//    console.log($(this).attr('id'));
+ //  })
